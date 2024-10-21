0e9b182eeb1de00aedd3dd84e33efc0aad0b2e17
Ali Hamza Sultan <122203689+alihamzasultan@users.noreply.github.com>
2024-10-21 21:15:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ window.addEventListener('beforeunload', function () {
 });
 async function sendQueryToServer(queryText) {
     try {
-        const response = await fetch('http://localhost:3000/chat', {
+        const response = await fetch('https://animation-bot-production.up.railway.app/chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
